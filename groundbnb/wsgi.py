e6c8ecebab477b635081b70d0dc4d64c976bce78
yu <yu@yuui-MacBookPro.local>
2021-06-23 20:55:28 +0900

    Modify: setting -> settings 폴더명 변경, 모델 객체명 변경

--- groundbnb/wsgi.py
+++ groundbnb/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'groundbnb.setting.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'groundbnb.settings.local')
 
 application = get_wsgi_application()
