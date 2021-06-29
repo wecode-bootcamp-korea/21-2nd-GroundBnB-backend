36b1508627b6832385255ada1c0832efb36a5120
SSABOODA <69753846+SSABOODA@users.noreply.github.com>
2021-06-29 19:57:30 +0900

    refactor: 리뷰 반영

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 import json, jwt, requests
-from os import access
 
 from django.views     import View
 from django.http      import JsonResponse
@@ -7,39 +6,65 @@ from django.http      import JsonResponse
 from groundbnb.settings.local import LOCAL_SECRET_KEY, ALGORITHM
 from users.models             import User, SocialFlatform
 
+class KakaoAPI:
+    def __init__(self, token):
+        self.token = token
+    
+    def get_kakao_user(self):
+        kakao_userinfo_url = "https://kapi.kakao.com/v2/user/me"
+        headers            = {"Authorization" : f"Bearer {self.token}"}
+        
+        response = requests.get(kakao_userinfo_url, headers = headers, timeout=1000) 
+        
+        if response.status_code != 200:
+            return JsonResponse({'MESSAGE' : 'RESPONSE_ERROR'}, status=400)
+
+        return response.json()
+
+    def request_kakao_logout(self):
+        kakao_logout_url = 'https://kapi.kakao.com/v1/user/logout'
+        headers          = {"Authorization" : f"Bearer {self.token}"}
+
+        response = requests.get(kakao_logout_url, headers = headers)
+
+        if response.status_code != 200:
+            return JsonResponse({'MESSAGE' : 'RESPONSE_ERROR'}, status=400)
+        
+        return response.json()
+
+class GoogleAPI:
+    def __init__(self, token):
+        self.token = token
+
+    def get_google_user(self):
+        google_userinfo_url = 'https://www.googleapis.com/oauth2/v2/userinfo?access_token='
+        response    = requests.get(f'{google_userinfo_url}{self.token}')
+
+        if response.status_code != 200:
+            return JsonResponse({'MESSAGE' : 'RESPONSE_ERROR'}, status=400)
+            
+        return response.json()
+
 class KakaoLoginView(View):
     def get(self, request):
         try:
-            kakao_token = request.headers.get('Authorization')
-            api_url     = "https://kapi.kakao.com/v2/user/me"
-            token_type  = "Bearer"
+            access_token = request.headers.get('Authorization')
 
-            if not kakao_token:
-                return JsonResponse({'MESSAGE': "TOKEN REQUIRED"}, status=400)
+            if not access_token:
+                return JsonResponse({'MESSAGE': 'TOKEN REQUIRED'}, status=400)
 
-            kakao_user_info = requests.get(api_url, headers= {'Authorization':f'{token_type} {kakao_token}'}).json()
-            error_massage   = kakao_user_info.get('msg', None)
+            kakao_user      = KakaoAPI(access_token)            
+            kakao_user_info = kakao_user.get_kakao_user()
+            kakao_account   = kakao_user_info.get('kakao_account')
+            profile         = kakao_account.get('profile')
             
-            if error_massage:
-                return JsonResponse({'MESSAGE' : error_massage}, status=400)
-
-            kakao_account = kakao_user_info.get("kakao_account", None)
-            profile = kakao_account.get("profile", None)
-            
-            kakao_provider_id       = str(kakao_user_info.get('id')) 
-            social_user             = SocialFlatform.objects.all()
-            social_user_provider_id = []
-            
-            for i in social_user:
-                social_user_provider_id.append(i.provider_id)
-
-            if kakao_provider_id not in social_user_provider_id:
-                user = User.objects.create(
-                    name  = profile.get('nickname'),
-                    email = kakao_account.get('email'),
-                    birth = kakao_user_info.get('birth', None),
-                )
-
+            user, created = User.objects.get_or_create(
+                name  = profile.get('nickname'),
+                birth = profile.get('birth', None),
+                email = kakao_account.get('email')
+            )
+        
+            if created:
                 SocialFlatform.objects.create(
                     provider_name = 'kakao',
                     provider_id   = kakao_user_info.get('id'), 
@@ -47,37 +72,46 @@ class KakaoLoginView(View):
                     nick_name     = profile.get('nickname'),
                     user          = user
                 )
+                user.name  = profile.get('nickname')
+                user.birth = profile.get('birth', None)
+                user.email = kakao_account.get('email')
+                user.save()
+                
                 social_user_id = SocialFlatform.objects.get(user_id=user.id)           
                 access_token   = jwt.encode({'id': social_user_id.id, 'is_host' : user.host}, LOCAL_SECRET_KEY, algorithm=ALGORITHM)
 
                 return JsonResponse({'MESSAGE': 'CREATED_USER', 'access_token': access_token}, status=200)
-           
-            user         = SocialFlatform.objects.get(provider_id=kakao_provider_id)
+
+            kakao_id     = kakao_user_info.get('id')
+            user         = SocialFlatform.objects.get(provider_id=kakao_id)
             is_host      = user.user.host
             access_token = jwt.encode({'id': user.id, 'is_host' : is_host}, LOCAL_SECRET_KEY, algorithm=ALGORITHM)           
 
             return JsonResponse({'MESSAGE': 'EXISTS_USER', 'access_token': access_token}, status=200)
-
         except KeyError:
             return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+class KakaoLogoutView(View):
+    def post(self, request):
+        access_token = request.headers.get('Authorization')
+        kakao_user   = KakaoAPI(access_token)
+        user_logout  = kakao_user.request_kakao_logout()
+        
+        return JsonResponse({'MESSAGE' : '로그아웃 되셨습니다아아.', 'token' : user_logout}, status=200)
 
 class GoogleLoginView(View):
     def get(self, request):
         try:
             access_token = request.headers.get('Authorization')
-            url = 'https://www.googleapis.com/oauth2/v2/userinfo?access_token='
-
-            response         = requests.get(url+access_token)
-            google_user_info = response.json()
 
             if not access_token:
                 return JsonResponse({'MESSAGE': 'TOKEN REQUIRED'}, status=400)
             
-            # DB에 데이터 없을 때 신규회원으로 인식하고 회원가입
+            google_login_api       = GoogleAPI(access_token)
+            google_user_info       = google_login_api.get_google_user()
             google_user_info_email = google_user_info.get('email')
             google_provider_id     = google_user_info.get('id')
 
-            ## 지정 scope 에 따라 응답 사용자 정보가 다름.
             if not User.objects.filter(email=google_user_info_email).exists():
                 user = User.objects.create(
                     name  = '',
